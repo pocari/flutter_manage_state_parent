eb63cc75e700aa5017a9c45d76c01c284f7c38c1
pocari <caffelattenonsugar@gmail.com>
2019-11-10 14:11:10 +0900

    親ウィジェットで状態管理

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      title: 'Navigation',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TapBox'),
+        ),
+        body: Center(child: ParentWidget()),
+      ),
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ParentWidget extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {}
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  Widget build(BuildContext context) => Container(
+        child: TapBoxB(
+          active: _active,
+          onChanged: _handleTapBoxChanged,
+        ),
+      );
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  TapBoxB({Key key, this.active: false, @required this.onChanged})
+      : assert(onChanged != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: _handleTap,
+        child: Container(
+          child: Center(
+            child: Text(
+              active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white),
+            ),
+          ),
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600],
+          ),
+        ),
+      );
+
+  void _handleTap() {
+    onChanged(!active);
+  }
 }
