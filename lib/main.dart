29b3a3e007fbaa623156dd0349b77592dc33e36b
pocari <caffelattenonsugar@gmail.com>
2019-11-10 14:29:22 +0900

    親子両方で状態管理

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _ParentWidgetState extends State<ParentWidget> {
   bool _active = false;
 
   Widget build(BuildContext context) => Container(
-        child: TapBoxB(
+        child: TapBoxC(
           active: _active,
           onChanged: _handleTapBoxChanged,
         ),
@@ -29,6 +29,71 @@ class _ParentWidgetState extends State<ParentWidget> {
     setState(() {
       _active = newValue;
     });
+  }
+}
+
+class TapBoxC extends StatefulWidget {
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  TapBoxC({Key key, this.active, @required this.onChanged})
+      : assert(onChanged != null),
+        super(key: key);
+
+  @override
+  _TapBoxCState createState() => _TapBoxCState();
+}
+
+class _TapBoxCState extends State<TapBoxC> {
+  bool _highlight = false;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: _handleTap,
+        onTapDown: _handleTapDown,
+        onTapUp: _handleTapUp,
+        onTapCancel: _handleTapCancel,
+        child: Container(
+          child: Center(
+            child: Text(
+              widget.active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white),
+            ),
+          ),
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+            border: _highlight
+                ? Border.all(
+                    color: Colors.teal[700],
+                    width: 10.0,
+                  )
+                : null,
+          ),
+        ),
+      );
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
   }
 }
 
